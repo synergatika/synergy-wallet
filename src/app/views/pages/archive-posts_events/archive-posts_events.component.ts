c4293f02b68e5721a43f5733e6e920955f04d1bf
Dimitris Mytakis <dmytakis@gmail.com>
2021-07-13 15:23:30 +0300

    Split Posts Lists

--- src/app/views/pages/archive-posts_events/archive-posts_events.component.ts
+++ src/app/views/pages/archive-posts_events/archive-posts_events.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, OnDestroy, ChangeDetectorRef } from '@angular/core';
 import { Subject } from 'rxjs';
-import { tap, takeUntil, finalize } from 'rxjs/operators';
+import { tap, takeUntil, finalize, filter } from 'rxjs/operators';
 import { TranslateService } from '@ngx-translate/core';
 
 /**
@@ -9,6 +9,7 @@ import { TranslateService } from '@ngx-translate/core';
 import { ItemsService } from '../../../core/services/items.service';
 
 import { PostEvent } from 'sng-core';
+import { ActivatedRoute, NavigationStart, Params, Router } from '@angular/router';
 
 @Component({
 	selector: 'app-archive-posts_events',
@@ -21,6 +22,8 @@ export class ArchivePostsEventsComponent implements OnInit {
 	 * Content Variables
 	 */
 	public posts: PostEvent[] = [];
+
+	access: string = null;
 
 	counter: number = 0;
 
@@ -37,7 +40,8 @@ export class ArchivePostsEventsComponent implements OnInit {
 	constructor(
 		private cdRef: ChangeDetectorRef,
 		public translate: TranslateService,
-		private itemsService: ItemsService
+		private itemsService: ItemsService,
+		private route: ActivatedRoute
 	) {
 		this.unsubscribe = new Subject();
 	}
@@ -46,7 +50,11 @@ export class ArchivePostsEventsComponent implements OnInit {
 	 * On destroy
 	 */
 	ngOnInit() {
-		this.fetchPostsEventsData(this.counter);
+		//		this.fetchPostsEventsData(this.counter);
+		this.route.params.subscribe((params: Params) => {
+			this.access = params['access'];
+			if (!this.access) this.access = 'general';
+		});
 	}
 
 	/**
@@ -61,33 +69,33 @@ export class ArchivePostsEventsComponent implements OnInit {
 	/**
 	 * Fetch Post & Events List
 	 */
-	fetchPostsEventsData(counter: number) {
-		this.itemsService.readAllPrivatePostsEvents(`6-${counter.toString()}-0`)
-			.pipe(
-				tap(
-					data => {
-						this.posts = this.posts.concat(data);
-						console.log("all posts");
-						console.log(this.posts)
-					},
-					error => {
-					}),
-				takeUntil(this.unsubscribe),
-				finalize(() => {
-					this.loading = false;
-					this.cdRef.markForCheck();
-				})
-			)
-			.subscribe();
-	}
+	// fetchPostsEventsData(counter: number) {
+	// 	this.itemsService.readAllPrivatePostsEvents(`6-${counter.toString()}-0-${this.access === 'internal' ? '1' : '0'}`)
+	// 		.pipe(
+	// 			tap(
+	// 				data => {
+	// 					this.posts = this.posts.concat(data);
+	// 					console.log("all posts");
+	// 					console.log(this.posts)
+	// 				},
+	// 				error => {
+	// 				}),
+	// 			takeUntil(this.unsubscribe),
+	// 			finalize(() => {
+	// 				this.loading = false;
+	// 				this.cdRef.markForCheck();
+	// 			})
+	// 		)
+	// 		.subscribe();
+	// }
 
 	/**
 	 * On Scroll
 	 */
 	onScroll() {
-		this.counter = this.counter + 1;
-		this.fetchPostsEventsData(this.counter);
-		console.log('scrolled!!');
-		this.cdRef.markForCheck();
+		// this.counter = this.counter + 1;
+		// this.fetchPostsEventsData(this.counter);
+		// console.log('scrolled!!');
+		// this.cdRef.markForCheck();
 	}
 }
